4c1d13e125363a8b47952278adab266aa37af303
guiemi <guiemi.teixeira@gmail.com>
2018-06-04 01:46:52 -0300

    Atualizando comentários no arquivo

--- lauda.py
+++ lauda.py
@@ -1,7 +1,13 @@
 num_char = int(input("Caracteres (com espaço): \n"))
-tam_lauda = 1700 # Uma evolução do programa é criar uma caixa pro usuário poder ajustar o tamanho da lauda
-val_lauda = 4 # Uma outra evolução do programa é criar uma caixa pro usuário poder ajudstar o valor cobrado por lauda
+tam_lauda = 1700
+val_lauda = 4
 
 price = ((num_char / tam_lauda) * val_lauda)
 
 print("Preço estipulado: R$ %2.2f" %(price))
+
+# Uma evolução do programa é criar uma caixa
+# Pro usuário poder ajustar o tamanho da lauda.
+
+# Outra evolução é criar uma caixa
+# Pro usuário poder ajustar o valor cobrado por lauda.
